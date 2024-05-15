560217c5b714c971aadca52c3a60d4891230589c
ajay <agadjangarlyev@gmail.com>
2024-05-15 23:17:21 +0300

    add lab4

--- 4.1.py
+++ 4.1.py
@@ -1,47 +1,22 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from scipy.spatial import ConvexHull
+import numpy as np
 
-# Заданные значения
-mx, my, mz = 2, 2, 1
+# Задаем координаты точек
+points = np.array([[9, -9], [8, -4], [9, 5], [9, 10], [6, 5], [-4, 0], [1, 0]])
 
-# Вершины тетраэдра
-vertices = np.array([[0, 0, 0],
-                     [1, 0, 0],
-                     [0, 1, 0],
-                     [0, 0, 1]])
+# Строим выпуклую оболочку
+hull = ConvexHull(points)
 
-# Функция для построения аксонометрической проекции осей
-def plot_axes(ax):
-    ax.quiver(0, 0, 0, 1, 0, 0, color='r', arrow_length_ratio=0.1)
-    ax.quiver(0, 0, 0, 0, 1, 0, color='g', arrow_length_ratio=0.1)
-    ax.quiver(0, 0, 0, 0, 0, 1, color='b', arrow_length_ratio=0.1)
-    ax.text(1.1, 0, 0, 'X', color='r')
-    ax.text(0, 1.1, 0, 'Y', color='g')
-    ax.text(0, 0, 1.1, 'Z', color='b')
+# Строим график точек
+plt.plot(points[:,0], points[:,1], 'o')
 
-# Функция для построения тетраэдра
-def plot_tetrahedron(ax):
-    edges = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]
-    for edge in edges:
-        ax.plot3D(*zip(vertices[edge[0]], vertices[edge[1]]), color='black')
+# Строим границы выпуклой оболочки
+for simplex in hull.simplices:
+    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
 
-# Создание графика
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Построение осей и тетраэдра
-plot_axes(ax)
-plot_tetrahedron(ax)
-
-# Настройка графика
-ax.set_xlim(0, 1.5)
-ax.set_ylim(0, 1.5)
-ax.set_zlim(0, 1.5)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.view_init(elev=20., azim=-45)  # Угол обзора
-
-# Отображение графика
+plt.title('Выпуклая оболочка множества точек')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
 plt.show()
--- 4.2.py
+++ 4.2.py
@@ -1,55 +1,52 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import numpy as np
 
-# Направляющий вектор проектирования
-s = np.array([0, 1, 1])
 
-# Построение профильной плоскости
-profile_matrix = np.eye(3) - np.outer(s, s) / np.linalg.norm(s)
-profile_axes = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
-profile_projection = profile_axes @ profile_matrix
+def is_convex(p1, p2, p3):
+    # Функция для проверки выпуклости треугольника
+    det = (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])
+    return det >= 0
 
-# Построение фронтальной плоскости
-frontal_matrix = np.eye(3) - np.outer(s, s) / np.linalg.norm(s)
-frontal_axes = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-frontal_projection = frontal_axes @ frontal_matrix
 
-# Построение горизонтальной плоскости
-horizontal_matrix = np.eye(3) - np.outer(s, s) / np.linalg.norm(s)
-horizontal_axes = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
-horizontal_projection = horizontal_axes @ horizontal_matrix
+def divide_polygon(points):
+    triangles = []
+    # Триангуляция полигона
+    for i in range(1, len(points) - 1):
+        if is_convex(points[0], points[i], points[i+1]):
+            triangles.append([points[0], points[i], points[i+1]])
+        else:
+            triangles.append([points[0], points[i+1], points[i]])
 
-# Визуализация проекций
-fig = plt.figure()
+    return triangles
 
-# Профильная плоскость
-ax1 = fig.add_subplot(131, projection='3d')
-ax1.quiver(0, 0, 0, *s, color='r', label='s')
-ax1.quiver(0, 0, 0, *profile_projection[:, 0], color='g', label='x')
-ax1.quiver(0, 0, 0, *profile_projection[:, 1], color='b', label='y')
-ax1.set_xlabel('Oy\'')
-ax1.set_ylabel('Oz\'')
-ax1.set_zlabel('Ox\'')
-ax1.legend()
 
-# Фронтальная плоскость
-ax2 = fig.add_subplot(132, projection='3d')
-ax2.quiver(0, 0, 0, *s, color='r', label='s')
-ax2.quiver(0, 0, 0, *frontal_projection[:, 0], color='g', label='x')
-ax2.quiver(0, 0, 0, *frontal_projection[:, 1], color='b', label='y')
-ax2.set_xlabel('Ox\'')
-ax2.set_ylabel('Oy\'')
-ax2.set_zlabel('Oz\'')
-ax2.legend()
+def plot_polygon(points, triangles):
+    fig, ax = plt.subplots()
+    patches = []
 
-# Горизонтальная плоскость
-ax3 = fig.add_subplot(133, projection='3d')
-ax3.quiver(0, 0, 0, *s, color='r', label='s')
-ax3.quiver(0, 0, 0, *horizontal_projection[:, 0], color='g', label='x')
-ax3.quiver(0, 0, 0, *horizontal_projection[:, 1], color='b', label='y')
-ax3.set_xlabel('Oz\'')
-ax3.set_ylabel('Ox\'')
-ax3.set_zlabel('Oy\'')
-ax3.legend()
+    # Отображение полигона
+    polygon = Polygon(points, closed=True, edgecolor='black', facecolor='none')
+    ax.add_patch(polygon)
 
-plt.show()
+    # Отображение треугольников
+    for triangle in triangles:
+        patches.append(Polygon(triangle, closed=True, edgecolor='blue', facecolor='lightblue'))
+
+    p = PatchCollection(patches, match_original=True)
+    ax.add_collection(p)
+
+    ax.autoscale_view()
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+
+# Заданные точки полигона
+points = np.array([[0, 0], [-3, 2], [-1, 4], [0, 2], [3, 3], [1, 2], [12, 1], [15, 85]])
+
+# Триангуляция полигона
+triangles = divide_polygon(points)
+
+# Визуализация
+plot_polygon(points, triangles)
